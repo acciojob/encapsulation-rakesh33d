da524747368ece01c2f5c4e57924d9ee41073bcd
Rakesh <rakesh33d@gmail.com>
2023-01-21 09:45:16 +0530

    sol2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,7 @@ package com.driver;
 public class Main {
 
     public static void main(String args[]){
+        //this is a change
         RWOnly obj = new RWOnly();
         obj.setName("rakesh");
         obj.getName();
